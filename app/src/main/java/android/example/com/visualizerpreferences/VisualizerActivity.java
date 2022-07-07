435375b3e85de4e8d2602007afa8f2d93a2d1786
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-07 09:13:16 +0530

    changed if to else if.

--- app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -68,11 +68,9 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals(getString(R.string.pref_show_bass_key))) {
             mVisualizerView.setShowBass(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_show_bass_default)));
-        }
-        if(key.equals(getString(R.string.pref_show_mid_range_key))){
+        }else if(key.equals(getString(R.string.pref_show_mid_range_key))){
             mVisualizerView.setShowMid(sharedPreferences.getBoolean(key,getResources().getBoolean(R.bool.pref_show_mid_range_default)));
-        }
-        if(key.equals(getString(R.string.pref_show_treble_key))){
+        }else if(key.equals(getString(R.string.pref_show_treble_key))){
             mVisualizerView.setShowTreble(sharedPreferences.getBoolean(key,getResources().getBoolean(R.bool.pref_show_treble_default)));
         }
     }
